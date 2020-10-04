8112e28f8f8a623c34512def08e60da0c2557a63
changhoS <ckdgh940@naver.com>
2020-10-04 17:06:17 +0900

    no message

---
+++ Navigation/Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private Camera cam;
+    [SerializeField] private LayerMask layer;
+    [SerializeField] private float speed; //속도
+    [SerializeField] private float angular_speed;
+    [SerializeField] private float acceleration_speed;
+    private NavMeshAgent _agent;
+
+    private void Awake()
+    {
+
+        _agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    private void Start()
+    {
+
+        _agent.speed = speed;
+        _agent.angularSpeed = angular_speed;
+        _agent.acceleration = acceleration_speed; 
+
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            var mousePos = Input.mousePosition;
+            var ray = cam.ScreenPointToRay(mousePos);
+           if( Physics.Raycast(ray, out var raycastHit,layer))
+            {
+                _agent.SetDestination(raycastHit.point);
+
+            }
+        }
+    }
+
+}
---
+++ Navigation/Assets/Script/Player2D.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Player2D : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 10f;
+    private Seeker _seeker;
+    private void Awake()
+    {
+        _seeker = GetComponent<Seeker>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PathRoutin());
+
+    }
+
+    IEnumerator PathRoutin()
+    {
+        var index = 0;
+        var offset = 0.1f;
+        var path = _seeker.StartPath(transform.position, target.position);
+
+
+        yield return path.WaitForPath();
+
+
+        foreach (var pos in path.vectorPath)
+        {
+            var targetPos = pos;
+            var vec = targetPos - transform.position;
+
+            while (this.enabled)
+            {
+                transform.Translate(vec.normalized * Time.deltaTime * speed);
+
+                var dist = vec.magnitude;
+
+                if (dist <= offset)
+                {
+                    index++;
+                    yield break;
+                }
+
+                yield return null;
+
+            }
+
+        }
+    }
+
+}
